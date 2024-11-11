4df221934aefc9ba7453baf97d515f8d2bd4ff5e
jh-210 <dks66092487@gmail.com>
2024-11-11 15:32:06 +0900

    애니메이션 및 배경 이동

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,22 +5,45 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
-    public float speed;
+    [SerializeField] private float speed = 2.3f;
+    public bool isRight = true;
+
 
     Rigidbody2D rigid;
+    SpriteRenderer spriteR;
+    Animator animator;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteR = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+
     }
 
     private void FixedUpdate()
     {
         rigid.linearVelocity = inputVec * speed;
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVec.magnitude);
+
+        if (isRight && inputVec.x < 0)
+        {
+            spriteR.flipX = true;
+            isRight = false;
+        }
+        else if (!isRight && inputVec.x > 0)
+        {
+            spriteR.flipX = false;
+            isRight = true;
+        }
     }
 }
