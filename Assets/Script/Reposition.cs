f10e8e0475a34db85c0726253065ea0b1cd35bf5
jh-210 <dks66092487@gmail.com>
2024-11-18 10:04:25 +0900

    적 관련
    적이 따라오고 화면에서 나가지면 다시 위치 이동

--- Assets/Script/Reposition.cs
+++ Assets/Script/Reposition.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D collider2D;
+
+    private void Awake()
+    {
+        collider2D = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Area"))
+        if (!collision.CompareTag("Area") || !collision.CompareTag("Enemy"))
             return;
 
         Vector3 playerPos = GameManager.instance.player.transform.position;
@@ -15,7 +22,7 @@ public class Reposition : MonoBehaviour
 
         Vector3 playerDir = GameManager.instance.player.inputVec;
 
-        float dirX = GameManager.instance.player.isRight ? 1 : -1;
+        float dirX = playerDir.x < 0 ? -1 : 1;
         float dirY = playerDir.y < 0 ? -1 : 1;
 
         switch(transform.tag)
@@ -33,7 +40,11 @@ public class Reposition : MonoBehaviour
                 break;
 
             case "Enemy":
-
+                if(collider2D.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0));
+                }
+                
                 break;
         }
     }
